4c3260adb22692c455c1f6bf4e980a1543495b47
JackoDev <untaljacko@gmail.com>
2021-03-19 22:24:46 -0500

    refactoring - create a partial for form and for messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was succesfully created!"
       redirect_to article_path(@article)
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was edited!"
       redirect_to article_path(@article)
     else
@@ -43,6 +43,10 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
