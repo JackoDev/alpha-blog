b984078be6bf6687ea389687e91df899949dd503
JackoDev <untaljacko@gmail.com>
2021-03-22 11:35:00 -0500

    association beetwen users and articles and validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.last # hardcode temporarly for allow create articles from the browser
     if @article.save
       flash[:notice] = "Article was succesfully created!"
       redirect_to article_path(@article)
